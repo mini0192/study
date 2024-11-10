6fb1c36939c768beff3f4fd073323a028c30daa2
mini <155148431+mini0192@users.noreply.github.com>
2024-11-10 10:55:13 +0900

    class

--- 수업/Java/class1/src/main/java/com/example/application/data/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.application.data;
-
-import com.example.application.data.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
